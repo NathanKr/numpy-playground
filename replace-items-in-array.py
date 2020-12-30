3cd0cd426a9e5c671c00104de03630410b4a59f9
Nathan Krasney <natankrasney@gmail.com>
2020-12-30 21:55:38 +0200

    ...

--- replace-items-in-array.py
+++ replace-items-in-array.py
@@ -2,8 +2,8 @@ import numpy as np
 
 m=10
 ar1 = np.arange(m)
-print("ar1 befor\n",ar1)
-ar2 = 5* np.ones(m)
+print("ar1 before\n",ar1)
+ar2 = 10 * np.ones(m)
 
 # replace all items above 5 by 10
 ar1 = np.where(ar1 > 5 ,ar2, ar1)
