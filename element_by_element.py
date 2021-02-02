c2b15053d0c121122426653edf9b150c8be3091c
Nathan Krasney <natankrasney@gmail.com>
2021-02-02 19:26:52 +0200

    element by element

---
+++ element_by_element.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+v1 = np.array([1,2,3])
+print(f"v1 : {v1}")
+
+v2 = np.array([4,5,6])
+print(f"v2 : {v2}")
+
+print(f"v1+v2 : {v1+v2}")
+print(f"v1-v2 : {v1-v2}")
+print(f"v1*v2 : {v1*v2}")
+print(f"v1/v2 : {v1/v2}")
+
+
+
+
+
