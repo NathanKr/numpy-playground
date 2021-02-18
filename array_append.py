8a94b79888e4b4a6988298ee56eb2b1e1601b21a
Nathan Krasney <natankrasney@gmail.com>
2021-02-18 10:57:14 +0200

    more append

--- array_append.py
+++ array_append.py
@@ -6,23 +6,44 @@ ar = np.append(ar,1)
 ar = np.append(ar,2) 
 print(ar)
 
-# append 1D list\array to 1D array
+# append 1D list\array to 1D array , flatten
 ar = np.array([])
 ar = np.append(ar,[1,2,3]) 
 ar = np.append(ar,[4,5,6]) 
 print(ar)
 
-# append array to 2D array
+# append 1D array to 2D array , no axis
+
 ar = np.array([[1,2,3]])
 ar = np.append(ar,[[4,5,6]]) 
 print(ar)
 
-# append array to 2D array
+# append 1D array to 2D array , axis=0
 ar = np.array([[1,2,3]])
 ar = np.append(ar,[[4,5,6]],axis=0) 
+print(ar.shape)
 print(ar)
 
-# append array to 2D array
+# append 1D array to 2D array , axis=1
 ar = np.array([[1,2,3]])
 ar = np.append(ar,[[4,5,6]],axis=1) 
+print(ar)
+
+# append 1D array to 2D array , vstack
+ar = np.array([1,2,3])
+ar = np.vstack((ar,[4,5,6])) 
+print(ar.shape)
+print(ar)
+
+# append 1D array , hstack
+ar = np.array([1,2,3])
+ar = np.hstack((ar,[4,5,6])) 
+print(ar.shape)
+print(ar)
+
+# append 1D array to 2D array , insert
+ar = np.zeros((2,3))
+ar[0,:] = [1,2,3]
+ar[1,:] = [4,5,6]
+print(ar.shape)
 print(ar)
