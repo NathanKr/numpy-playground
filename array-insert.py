c7b9c5a8029057deeb0149d2e9abfeed6619f68f
Nathan Krasney <natankrasney@gmail.com>
2021-02-19 12:41:05 +0200

    insert also array

--- array-insert.py
+++ array-insert.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 ar = np.array([1,2,3])
-ar1 = np.insert(ar,1,7) #add 7 before index 1 of the input array ar
+#add 7 before index 1 of the input array ar
+ar1 = np.insert(ar,1,7) 
+print(f"ar : {ar}")
+print(f"ar1 : {ar1}")
+
+ar = np.array([1,2,3])
+#add 7,8,9 before index 1 of the input array ar
+ar1 = np.insert(ar,1,[7,8,9]) 
 print(f"ar : {ar}")
 print(f"ar1 : {ar1}")
