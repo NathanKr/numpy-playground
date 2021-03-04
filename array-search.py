5c41a6f6a99e45923ca805e76ecd197cdcc59352
Nathan Krasney <natankrasney@gmail.com>
2021-03-04 14:32:56 +0200

    where 2D

--- array-search.py
+++ array-search.py
@@ -10,5 +10,10 @@ result = np.where(arr_1d == 14)
 print("result not found : ",result)
 print("result[0] : ",result[0])
 
+arr_2d = np.array([[1, 2, 3],[4 , 5, 2]])
+result = np.where(arr_2d == 2)
+print(f"result\n{result}")
+
+
 
 
